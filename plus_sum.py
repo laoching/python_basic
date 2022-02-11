6727a9357d1bc19ad6b58402b6a6cb7cad787044
sangmin <wjdtkdals159@gmail.com>
2022-02-12 00:08:42 +0900

    first commit

--- plus_sum.py
+++ plus_sum.py
@@ -1,12 +1,26 @@
-# 1부터 n까지의 합 구하기(n>0)
+# 10번 안에 숫자 맞추기
 
-while True:
-    n = int(input("n을 입력하세요: "))
-    if n > 0:
+import random
+
+cnt = 10
+#1부터 1000까지의 난수 생성
+ran = random.randrange(1, 1000)
+while cnt > 0:
+    n = int(input("input number: "))
+    if n < 1 or n > 1000:
+        print("wrong number!")
+        print("chance: ",cnt)
+        continue
+    if n > ran:
+        cnt -= 1
+        print("not correct!! input smaller number")
+        print("chance: ",cnt)
+    elif n < ran:
+        cnt -= 1
+        print("not correct!! input bigger number")
+        print("chance: ",cnt)
+    elif n == ran:
+        print("random number:", n)
+        print("program End")
         break
-sum = 0
-i = 1
-while i <= n:
-    sum += i
-    i += 1
-print('1부터 n까지의 합: ',sum)
+print("answer is: ", ran)
