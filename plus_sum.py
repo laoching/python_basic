b3d40d788b5ba752c7e4fb2a9e370fcd81c4083b
sangmin <wjdtkdals159@gmail.com>
2022-02-13 18:07:07 +0900

    second commit

--- plus_sum.py
+++ plus_sum.py
@@ -3,7 +3,9 @@
 import random
 
 cnt = 10
-#1부터 1000까지의 난수 생성
+#1부터 999까지의 난수 생성
+#randrange(a,b) -> a부터 b-1까지의 랜덤한 숫자
+#randint(a,b) -> a부터 b까지의 랜덤한 숫자
 ran = random.randrange(1, 1000)
 while cnt > 0:
     n = int(input("input number: "))
