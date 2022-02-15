a5c1c7ddf8bacffbb9f6ed96f43dc0ccdeb56da1
sangmin <wjdtkdals159@gmail.com>
2022-02-15 23:01:47 +0900

    second commit

--- calc.py
+++ calc.py
@@ -5,22 +5,26 @@
 # 연산을 수행하고 결과를 출력하는 calc 함수
 def calc(first, user_oper, second):
     if user_oper == '+':
-        print(first+second)
+        return first+second
     elif user_oper == '-':
-        print(first-second)
+        return first-second
     elif user_oper == '*':
-        print(first*second)
+        return first*second
     elif user_oper == '/':
-        print(first/second)
+        return first/second
 
-# 계산을 원하는 식을 입력(ex: 3+3, 1*2...)
-formula = input()
-oper = ['+', '-', '*', '/']
+def main():
+    # 계산을 원하는 식을 입력(ex: 3+3, 1*2...)
+    formula = input()
+    oper = ['+', '-', '*', '/']
 
-for i in formula:
-    if i in oper:
-        location = formula.index(i)
-        user_oper = i
-first = int(formula[:location])
-second = int(formula[location+1:])
-calc(first, user_oper, second)
+    for i in formula:
+        if i in oper:
+            location = formula.index(i)
+            user_oper = i
+    first = int(formula[:location])
+    second = int(formula[location+1:])
+    print(calc(first, user_oper, second))
+
+if __name__ == '__main__':
+    main()
